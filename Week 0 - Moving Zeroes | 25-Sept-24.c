3858def2b23416bac50599e059dda2902ab36e3d
pranay-kommuri <pranaykumar.cs23@bmsce.ac.in>
2024-10-23 05:26:54 +0530

    Week 0 - Moving Zeroes | 25-Sept-24.c
    Leetcode function that moves all the zeroes in an array to the right

--- Week 0 - Moving Zeroes | 25-Sept-24.c
+++ Week 0 - Moving Zeroes | 25-Sept-24.c
@@ -1,3 +1,7 @@
+
+// Week 0 - Moving Zeroes | 25-Sept-24
+// Leetcode function that moves all the zeroes in an array to the right
+
 void moveZeroes(int* nums, int numsSize) {
     int n=0;
     for(int i=0; i<numsSize; i++){
